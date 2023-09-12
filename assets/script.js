36f1d6c003af88e5855eda132a0015b7dd26d929
MS7BOK <maxsebok04@gmail.com>
2023-09-12 15:01:43 +1000

    added another function to show whether an answer is correct or incorrect and some if and else statements within. Also added a startTimer function and a end quiz function and a function to save all the scores aswell as a hide element and show element function

--- assets/script.js
+++ assets/script.js
@@ -71,3 +71,106 @@ function loadQuestion() {
     questionElement.textContent = question;
     choicesElement.innerHTML = choices.map(choice => `<li>${choice}</li>`).join('');
 }
+
+function handleAnswer(event) {
+    if (event.target.matches("li")) {
+        const selectedAnswer = event.target.textContent;
+        const correctAnswer = questions[currentQuestionIndex].correctAnswer;
+
+        if (selectedAnswer === correctAnswer) {
+            score++;
+            answerFeedbackElement.textContent = "Correct!";
+            answerFeedbackElement.classList.remove("incorrect-answer"); // Removes incorrect class
+            answerFeedbackElement.classList.add("correct-answer"); // Adds correct class
+            
+        } else {
+            timeLeft -= 10;
+            answerFeedbackElement.textContent = "Incorrect!";
+            answerFeedbackElement.classList.remove("correct-answer"); // Removes correct class
+            answerFeedbackElement.classList.add("incorrect-answer"); // Adds incorrect class
+        }
+        }
+
+        answerFeedbackElement.style.display = "block";
+
+        setTimeout(function () {
+            answerFeedbackElement.style.display = "none";
+        }, 1500);
+
+        currentQuestionIndex++;
+
+        if (currentQuestionIndex < questions.length) {
+            loadQuestion();
+        } else {
+            endQuiz();
+        }
+    }
+
+
+function startTimer() {
+    timerInterval = setInterval(function() {
+        timeLeft--;
+        timeLeftElement.textContent = timeLeft;
+
+        if (timeLeft <= 0) {
+            endQuiz();
+        }
+    }, 1000);
+}
+
+function endQuiz() {
+    clearInterval(timerInterval);
+    hideElement(quizScreen);
+    showElement(gameOverScreen);
+    finalScoreElement.textContent = score;
+    showElement(initialsInput);
+    showElement(submitScoreButton);
+}
+
+let savedScores = []; 
+
+function saveScore() {
+    const initials = initialsInput.value.trim();
+
+    if (initials !== "") {
+        const savedScore = { initials, score };
+        savedScores = JSON.parse(localStorage.getItem("scores")) || [];
+        savedScores.push(savedScore);
+        localStorage.setItem("scores", JSON.stringify(savedScores));
+
+        currentQuestionIndex = 0;
+        timeLeft = 60;
+        score = 0;
+        initialsInput.value = "";
+        hideElement(gameOverScreen);
+    }
+
+    hideElement(quizScreen);
+    hideElement(gameOverScreen);
+    showElement(highscoreScreen);
+    highscore.textContent = savedScores.map(item => item.initials + ": " + item.score).join("\n");
+
+    clearButton.addEventListener("click", clearHighScores);
+    function clearHighScores() {
+        localStorage.removeItem("scores");
+    
+        savedScores = [];
+    
+        highscore.textContent = "";
+    
+    }
+
+}
+
+
+playAgainButton.addEventListener("click", startQuiz);
+
+function hideElement(element) {
+    element.classList.add("hidden");
+}
+
+function showElement(element) {
+    element.classList.remove("hidden");
+}
+
+
