f922d62a54bc0aa16056a48240a37333f04be2d7
MS7BOK <maxsebok04@gmail.com>
2023-09-12 14:49:56 +1000

    added questions for my quiz

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Language", "Highly Textured Markup Language", "Hyperlinks and Text Markup Language"],
+        correctAnswer: "Hyper Text Markup Language"
+    },
+    {
+        question: "What does JavaScript do?",
+        choices: ["Allows web pages to become interactive", "Help you get a prescription for certain types of coffee", "Reads from files in the client"],
+        correctAnswer: "Allows web pages to become interactive"
+    },
+    {
+        question: "What is CSS used for?",
+        choices: ["Creating dynamic content", "Styling web pages", "Handling user input"],
+        correctAnswer: "Styling web pages"
+    },
+    {
+        question: "Which programming language is known for its flexibility and ease of use?",
+        choices: ["Python", "Java", "C++"],
+        correctAnswer: "Python"
+    },
+    {
+        question: "Who is the inventor of javascript?",
+        choices: ["Brendan Eich", "James Gosling", "Yukihiro Matsum"],
+        correctAnswer: "Brendan Eich"
+    },
+    {
+        question: "what does getElementById do?",
+        choices: ["Displays an elements Id","Returns an Element object representing the element whose id property matches the specified string","Gets any CSS element by simply providing there Id tag"],
+        correctAnswer: "Returns an Element object representing the element whose id property matches the specified string",
+    },
+
+
+];
