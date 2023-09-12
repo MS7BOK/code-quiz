832747227b227e388511d8792d07823dee7e70ed
MS7BOK <maxsebok04@gmail.com>
2023-09-12 14:55:56 +1000

    added all my const elements event listeners and the start quiz function and load question function

--- assets/script.js
+++ assets/script.js
@@ -32,3 +32,42 @@ const questions = [
 
 
 ];
+
+let currentQuestionIndex = 0;
+let timeLeft = 60;
+let timerInterval;
+let score = 0;
+
+const startScreen = document.getElementById("start-screen");
+const quizScreen = document.getElementById("quiz-screen");
+const gameOverScreen = document.getElementById("game-over-screen");
+const startButton = document.getElementById("start-button");
+const questionElement = document.getElementById("question");
+const choicesElement = document.getElementById("choices");
+const answerFeedbackElement = document.getElementById("answer-feedback");
+const timeLeftElement = document.getElementById("time-left");
+const finalScoreElement = document.getElementById("final-score");
+const initialsInput = document.getElementById("initials");
+const submitScoreButton = document.getElementById("submit-score");
+const highscoreScreen = document.getElementById("highscore-screen");
+const highscore = document.getElementById("highscore"); 
+const clearButton = document.getElementById("clear-score");
+const playAgainButton = document.getElementById("play-again");
+
+startButton.addEventListener("click", startQuiz);
+choicesElement.addEventListener("click", handleAnswer);
+submitScoreButton.addEventListener("click", saveScore);
+
+function startQuiz() {
+    hideElement(startScreen);
+    hideElement(highscoreScreen);
+    showElement(quizScreen);
+    loadQuestion();
+    startTimer();
+}
+
+function loadQuestion() {
+    const { question, choices } = questions[currentQuestionIndex];
+    questionElement.textContent = question;
+    choicesElement.innerHTML = choices.map(choice => `<li>${choice}</li>`).join('');
+}
